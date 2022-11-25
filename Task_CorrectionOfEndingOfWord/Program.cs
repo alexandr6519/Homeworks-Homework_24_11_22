47892beddb7b136186106e175d7dbf34260b61d8
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-25 11:25:51 +0700

    adding of Task_CorrectionOfEndingOfWord

---
+++ Task_CorrectionOfEndingOfWord/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please, input the integer positive number!");
+int numberOfProgrammers = Convert.ToInt32(Console.ReadLine());
+if (numberOfProgrammers < 0)
+{
+    Console.WriteLine("You inputed the negative number!!! Please, input the integer positive number!");
+}
+else
+{
+    Console.Write(numberOfProgrammers);
+    int digitSecond;
+    if (numberOfProgrammers > 10)
+        digitSecond = (numberOfProgrammers / 10) % 10;
+    else digitSecond = 0;
+    if (numberOfProgrammers % 10 == 2 || numberOfProgrammers % 10 == 3 || numberOfProgrammers % 10 == 4 && digitSecond != 1)
+        Console.Write(" программиста");
+    else if (numberOfProgrammers % 10 == 1 && digitSecond != 1) Console.Write(" программист");
+    else Console.Write(" программистов");
+}
