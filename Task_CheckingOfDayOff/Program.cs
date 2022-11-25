cbe262b31e3b51d8d3dac9a18ac8c13fac727450
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-25 10:10:09 +0700

    adding of Task_Task_CheckingOfDayOff

---
+++ Task_CheckingOfDayOff/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberOfDay = new Random().Next(1, 8);
+if (numberOfDay == 6 || numberOfDay == 7) 
+Console.WriteLine($"This number {numberOfDay} stands for day off!");
+else Console.WriteLine($"This number {numberOfDay} stands for workday!");
